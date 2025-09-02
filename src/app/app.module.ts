641d5460e99fe768fda773c3f057f4c0d62a0311
vagnercruz <vagnerdacruz48@gmail.com>
2025-09-01 22:11:22 -0300

    feat: adiciona service para o consumo da API

---
+++ src/app/app.module.ts
@@ -0,0 +1,9 @@
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/services/usuarios.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Usuario{
+  id: string;
+  nome: string;
+  sobrenome: string;
+  idade: number;
+  profissao: string;
+  cidade: string;
+  estado: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuariosService {
+   private apiUrl = 'http://localhost:3333/usuarios';
+
+   constructor(private http: HttpClient) {}
+
+   listar(): Observable<Usuario[]> {
+     return this.http.get<Usuario[]>(this.apiUrl);
+   }
+
+   buscar(id: string): Observable<Usuario> {
+    return this.http.get<Usuario>(`${this.apiUrl}/${id}`);
+  }
+
+   criar(usuario: Usuario): Observable<Usuario> {
+    return this.http.post<Usuario>(this.apiUrl, usuario);
+  }
+
+  atualizar(id: string, usuario: Omit<Usuario, 'id'>): Observable<Usuario> {
+    return this.http.put<Usuario>(`${this.apiUrl}/${id}`, usuario);
+  }
+
+  remover(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}  
